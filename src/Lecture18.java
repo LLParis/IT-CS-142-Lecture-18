f29d5ab4a3d1a8478cf2f33f1afbbba1a42ce0af
Your Name <you@example.com>
2024-03-06 07:29:22 -0800

    Work In Progress

--- src/Lecture18.java
+++ src/Lecture18.java
@@ -7,8 +7,13 @@ public class Lecture18 {
     // Note: you will need to put some of your answers inside a comments
 
     // Put your answer for #22 here:
+    // An Interface is a contract stating that a class must implement methods from the interface it's implementing.
+    // Extending a class allows a subclass to inherit the attributes and methods of a superclass.
+    // Interfaces cant have state information while extended classes are allowed to.
+    // A class can implement multiple interfaces but inheritance can only extend one class.
 
     // Put your answer for #23 here:
+
 
     // Put your answer for #24 here:
 
